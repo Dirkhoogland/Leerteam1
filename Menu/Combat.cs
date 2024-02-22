d96cdb871b4a965ca2f09f534e4dc6350d563607
Dirkhoogland <99063413@mydavinci.nl>
2024-02-22 11:37:34 +0100

    finished healing, started on main combat function

--- Menu/Combat.cs
+++ Menu/Combat.cs
@@ -13,18 +13,42 @@ namespace Functions
     {
         public void StartCombat(Player player, Monster monster)
         {
-            
+            Weapon playerweapon = World.Weapons.id([player.CurrentWeapon]);
+            while(player.CurrentHitPoints > 0 && monster.CurrentHitPoints > 0)
+            {
+                
+            }
+            if (player.CurrentHitPoints <= 0)
+            {
+                Defeat();
+            }
+            else
+            {
+                Victory();
+            }
         }
-
+        
 
         public int AttackOpponent(int damage, int hp)
         {
-            return hp;
+            hp -= damage;
+            if (hp > 0)
+            {
+                return hp;
+            }
+            else 
+            { 
+                return hp;
+            }
+        }
+        public void Victory()
+        {
+            Console.WriteLine("You won the combat");
         }
 
         public void Defeat()
         {
-
+            Console.WriteLine("You lost the combat");
         }
 
         public int Healingplayer(int hp, int maxhp)
