9e78f30e6ab9f8efe9e5183d5fad5e4b22c85de5
Dirkhoogland <99063413@mydavinci.nl>
2024-02-22 11:08:14 +0100

    Create combat.cs

---
+++ Menu/Combat.cs
@@ -0,0 +1,40 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Functions
+{
+    internal class Combat
+    {
+        public void StartCombat(Player player, Monster monster)
+        {
+            
+        }
+
+
+        public int AttackOpponent(int damage, int hp)
+        {
+            return hp;
+        }
+
+        public void Defeat()
+        {
+
+        }
+
+        public int Healingplayer(int hp, int maxhp)
+        {
+            hp += 15;
+            if(hp > maxhp) hp = maxhp;
+
+
+            return hp;
+        }
+
+    }
+}
